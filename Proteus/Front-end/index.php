e51cbc45278af82eca3a81f6fe4907e452ca1475
cnbstvYuri <79946817+cnbstvYuri@users.noreply.github.com>
2021-05-24 20:59:43 -0300

    Create Imagens

---
+++ Proteus/Front-end/index.php
@@ -0,0 +1,42 @@
+<DOCTYPE HTML>
+
+    <head>
+        <title>PROTEUS E-STORE</title>
+        <meta charset = "UTF-8">
+        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="menu.css">
+    </head>
+
+    <body>
+        <header>
+
+            
+                <label class = "logo">Proteus e-commerce</label>
+                <nav class = "menu">
+                    <ul>
+                         <li><a href=index.html>Inicio</a></li>
+                         <li><a href=shop.html>shop</a></li>
+                         <li><a href=login.html>entrar</a></li>
+                         <li><a href=formtrab.html>Cadastrar-se</a></li>
+                    </ul>
+                </nav>
+
+            </header>
+
+            <div id = "novidades">
+                <h3>Confira as novidades!</h3>
+            </div>
+                
+            <img src = "camiseta.jpg" id = "ftcmst">
+
+            <div id = "bcknvds">
+        
+            </div>
+
+            <div id = "txtcmst">
+                <h3>Atualizações semanais! Clique e <br>
+                confira as novidades da loja!</h3>
+            </div>
+    </body>
+
+</html>
---
+++ Proteus/Front-end/shop.php
@@ -0,0 +1,41 @@
+<DOCTYPE HTML>
+<head>
+    <title>PROTEUS E-STORE</title>
+    <meta charset = "UTF-8">
+    <link rel="stylesheet" href="style3.css">
+    <link rel="stylesheet" href="menu.css">
+
+<body>
+    <header>
+
+            
+        <label class = "logo">Proteus e-commerce</label>
+        <nav class = "menu">
+            <ul>
+                 <li><a href=index.html>Inicio</a></li>
+                 <li><a href=shop.html>shop</a></li>
+                 <li><a href=login.html>entrar</a></li>
+                 <li><a href=formtrab.html>Cadastrar-se</a></li>
+            </ul>
+        </nav>
+
+    </header>
+    <div id = "c1">
+        <img src = "c1.png" id = "c1">
+    </div>
+    <div id = "c2">
+        <img src = "c2.jpg" id = "c1">
+    </div>
+    <div id = "c3">
+        <img src = "c3.jpg" id = "c3">
+    </div>
+    <div id = "c4">
+        <img src = "c4.jpg" id = "c4">
+    </div>
+    <p class = "pc4">R$ 160,00</p>
+    <p class = "pc3">R$ 179,00</p>
+    <p class = "pc2">R$ 149,90</p>
+    <p class = "pc1">R$ 200,00</p>
+</body>
+</head>
+</html>
